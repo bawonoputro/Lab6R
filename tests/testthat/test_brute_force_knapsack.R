c768fda2aeb2acd53f9ddbceb8431ad5e892ee80
Paripurna Bawonoputro <col.flamealchemist@gmail.com>
2025-10-10 12:42:30 +0200

    sanity

--- tests/testthat/test_brute_force_knapsack.R
+++ tests/testthat/test_brute_force_knapsack.R
@@ -3,7 +3,7 @@ suppressWarnings(RNGversion(min(as.character(getRversion()),"3.5.3")))
 set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
 
 # Test Data
-n <- 16
+n <- 2000
 knapsack_objects <- data.frame(
   w = sample(1:4000, size = n, replace = TRUE),
   v = runif(n = n, 0, 10000)
